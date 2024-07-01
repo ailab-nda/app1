46b9f72d4171a466673f6f05ef43df198b6f0b66
Hiroshi Sato <hsato@nda.ac.jp>
2024-07-01 11:09:25 +0900

    Initial Commit

---
+++ app1/app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by Hiroshi Sato on 2024/07/01.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
